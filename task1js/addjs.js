3dfc2d1c7348c9970632746aaa096f10d161ae96
mariamEnany <mariamenany@gmail.com>
2023-05-28 16:28:52 +0300

    first task

---
+++ task1js/addjs.js
@@ -0,0 +1,5 @@
+function add(){  
+    var number1=document.getElementById("num1").value;  
+    var number2=document.getElementById("num2").value;  
+    alert(parseFloat(number1)+parseFloat(number2));  
+    }  
